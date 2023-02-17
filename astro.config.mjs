e65719cb96ecddfe7812a05d7647352e444c1c47
Andrea Vouk <andrew.vouk@gmail.com>
2023-02-17 14:21:05 +0100

    Update styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,18 @@
 import { defineConfig } from 'astro/config';
-import sitemap from "@astrojs/sitemap";
+import sitemap from '@astrojs/sitemap';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
   site: 'https://andreavouk.com',
-  integrations: [sitemap()]
+  markdown: {
+    shikiConfig: {
+      theme: 'one-dark-pro',
+    }
+  },
+  integrations: [
+    sitemap(),
+    mdx(),
+  ]
 });
